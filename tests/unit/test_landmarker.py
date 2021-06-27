a9819fb5ade70983949c0bb27d5252eafe744bf5
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2021-06-27 10:36:09 +0500

    test, hooks, --showlocals

--- tests/unit/test_landmarker.py
+++ tests/unit/test_landmarker.py
@@ -1,5 +1,6 @@
 import os
 from code.extractors.landmarker import FacialPredictor
+from typing import Tuple
 
 import numpy as np
 import pytest
@@ -15,16 +16,36 @@ def single_face_image() -> np.ndarray:
         return data
 
 
+PREDICTORS = {
+    68: "shape_predictor_68_face_landmarks.dat",
+    5: "shape_predictor_5_face_landmarks.dat",
+}
+
+
+@pytest.fixture(params=[68, 5])
+def model(request):
+    i = request.param
+    return i, PREDICTORS[i]
+
+
 @pytest.fixture()
-def predictor():
-    model_file = "shape_predictor_68_face_landmarks.dat"
-    model_path = os.path.join(MODELS_DIR, model_file)
-    return FacialPredictor(model_path)
+def predictor_fixture(model: Tuple[int, str]) -> Tuple[int, FacialPredictor]:
+    count, file = model
+    model_path = os.path.join(MODELS_DIR, file)
+    return count, FacialPredictor(model_path)
 
 
 class TestFacialPredictor:
-    def test_yields_all_faces(
-        self, predictor: FacialPredictor, single_face_image
+    def test_yields_single_face(
+        self, predictor_fixture: Tuple[int, FacialPredictor], single_face_image
     ):
+        _, predictor = predictor_fixture
         marked_faces = list(predictor.get_landmarks(single_face_image))
         assert len(marked_faces) == 1
+
+    def test_has_all_landmarks(
+        self, predictor_fixture: Tuple[int, FacialPredictor], single_face_image
+    ):
+        expected_count, predictor = predictor_fixture
+        for face_landmarks in predictor.get_landmarks(single_face_image):
+            len(face_landmarks)
--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ import os
 
 from definitions import ROOT_DIR
 
-DATA_DIR = os.path.join('tests', 'data')
+DATA_DIR = os.path.join("tests", "data")
 TEST_DATA_DIR = os.path.join(ROOT_DIR, DATA_DIR)
 
 
